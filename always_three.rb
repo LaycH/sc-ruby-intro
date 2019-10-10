303466f0c66652fbeedf5f9a3d22557a66f7db90
Layc <youareastar11@gmail.com>
2019-10-09 19:17:05 -0600

    initial excercist "always 3 script"

---
+++ always_three.rb
@@ -0,0 +1,22 @@
+puts "Give me a number:"
+number = gets
+puts "Your number is #{number}."" 
+"number".to_i = 1 
++=5
+*=2
+-=4
+/=2
+
+print .to_s
+
+finalNumber = #
+
+puts "Your Final number is #{finalNumber}"
+
+
+
+
+
+
+
+
