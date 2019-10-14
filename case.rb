4ddece8413b1155f6a2d290e2b0ca3384da188a6
Layc <youareastar11@gmail.com>
2019-10-13 21:12:04 -0600

    case statment exercise

---
+++ case.rb
@@ -0,0 +1,10 @@
+check_weather = 'sunny'
+
+case check_weather
+when 'sunny'
+    puts "wear your flower sundress and big classy hat!"
+when 'rainy'
+    puts "wear cropped jeans, white blouse and blue sweater and umbrella!"
+when 'foggy'
+    puts "wear your blue sweater and booties."
+end
